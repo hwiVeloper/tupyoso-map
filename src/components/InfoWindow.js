eb4e06ec4b9cd25a56abc95131378f95a8bdcb3c
Jonghwi Lee <zziller03@gmail.com>
2020-04-15 16:07:55 +0900

    투표소명 수정

--- src/components/InfoWindow.js
+++ src/components/InfoWindow.js
@@ -1,18 +1,18 @@
 import React from "react";
 import "./InfoWindow.css";
 
-const InfoWindow = props => {
+const InfoWindow = (props) => {
   const placeInfo = props.data;
   return (
     <div class="placeinfo_wrap">
       <div class="placeinfo">
         <h6 class="title" href="" target="_blank">
-          {placeInfo.evPsName}
+          {placeInfo.psName}
         </h6>
         <span>{placeInfo.addr}</span>
         <span class="jibun">{placeInfo.placeName}</span>
         <span>{placeInfo.floor}</span>
-        <span class="tel">{placeInfo.evPsName}</span>
+        <span class="tel">{placeInfo.psName}</span>
       </div>
       <div class="after"></div>
     </div>
--- src/container/KakaoMapContainer.js
+++ src/container/KakaoMapContainer.js
@@ -9,7 +9,7 @@ const { kakao } = window;
 
 class KakaoMapContainer extends Component {
   state = {
-    isLoading: false
+    isLoading: false,
   };
 
   map;
@@ -22,18 +22,18 @@ class KakaoMapContainer extends Component {
     let el = document.getElementById("map");
     this.map = new kakao.maps.Map(el, {
       center: new kakao.maps.LatLng(36.61090580199106, 127.28712340471264),
-      level: 13
+      level: 13,
     });
     this.placeOverlay.setContent(this.contentNode);
 
     var po = this.placeOverlay;
 
-    kakao.maps.event.addListener(this.map, "zoom_changed", function() {
+    kakao.maps.event.addListener(this.map, "zoom_changed", function () {
       if (po.setMap) {
         po.setMap(null);
       }
     });
-    kakao.maps.event.addListener(this.map, "bounds_changed", function() {
+    kakao.maps.event.addListener(this.map, "bounds_changed", function () {
       if (po.setMap) {
         po.setMap(null);
       }
@@ -57,12 +57,12 @@ class KakaoMapContainer extends Component {
       var marker = new kakao.maps.Marker({
         map: this.map,
         position: new kakao.maps.LatLng(pollPlaces[i].y, pollPlaces[i].x),
-        clickable: true
+        clickable: true,
       });
 
       // click event
-      (function(marker, pollPlace, that) {
-        kakao.maps.event.addListener(marker, "click", function() {
+      (function (marker, pollPlace, that) {
+        kakao.maps.event.addListener(marker, "click", function () {
           that.displayPlaceInfo(pollPlace);
         });
       })(marker, pollPlaces[i], this);
@@ -73,9 +73,8 @@ class KakaoMapContainer extends Component {
 
   addMyLocationMarker(myLocation) {
     var imageSrc = myLoc;
-    var imageSize = new kakao.maps.Size(64, 64); // 마커이미지의 크기입니다
-    var imageOption = { offset: new kakao.maps.Point(27, 69) }; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
-    // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
+    var imageSize = new kakao.maps.Size(64, 64);
+    var imageOption = { offset: new kakao.maps.Point(27, 69) };
     var markerImage = new kakao.maps.MarkerImage(
       imageSrc,
       imageSize,
@@ -86,7 +85,7 @@ class KakaoMapContainer extends Component {
     var marker = new kakao.maps.Marker({
       map: this.map,
       position: new kakao.maps.LatLng(myLocation.y, myLocation.x),
-      image: markerImage
+      image: markerImage,
     });
 
     this.markers.push(marker);
@@ -118,10 +117,10 @@ class KakaoMapContainer extends Component {
         style={{ width: "100%", height: "100%" }}
       >
         <Controls
-          setZoomLevel={level => this.setZoomLevel(level)}
-          setCenter={coords => this.setCenter(coords)}
-          addMarker={pollPlaces => this.addMarker(pollPlaces)}
-          addMyLocationMarker={myLocation =>
+          setZoomLevel={(level) => this.setZoomLevel(level)}
+          setCenter={(coords) => this.setCenter(coords)}
+          addMarker={(pollPlaces) => this.addMarker(pollPlaces)}
+          addMyLocationMarker={(myLocation) =>
             this.addMyLocationMarker(myLocation)
           }
         />
